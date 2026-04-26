69d0614568eec9201b6b49b48b0fd1a7b8bfa4f8
omthakur1394 <your_main_github_email@gmail.com>
2026-04-26 09:19:40 +0530

    add the postsql

--- src/agent.py
+++ src/agent.py
@@ -4,11 +4,14 @@ from langchain_core.messages import AnyMessage
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.checkpoint.memory import MemorySaver
-from .config import llm 
-from .tools import tools
-
+from langgraph.checkpoint.postgres import PostgresSaver
+from config import llm 
+from dotenv import load_dotenv
+import os 
+from tools import tools
+load_dotenv ()
 llm_with_tools = llm.bind_tools(tools)
+DB_URI = os.getenv("DATABASE_URL")
 
 class State(TypedDict):
     messages:Annotated[list[AnyMessage],add_messages]
@@ -23,8 +26,7 @@ bulider.add_edge(START,"tools_calling")
 bulider.add_conditional_edges("tools_calling",tools_condition)
 bulider.add_edge("tools","tools_calling")
 
-memory = MemorySaver()
-
-graph = bulider.compile(checkpointer=memory)
-
+with PostgresSaver.from_conn_string(DB_URI) as checkpointer:
+    checkpointer.setup()
+    graph = bulider.compile(checkpointer=checkpointer)
 
